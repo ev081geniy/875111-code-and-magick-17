dea02c95a6b8a91e0f84b3c652fda038f8120d07
ev081geniy <ev081geniy@gmail.com>
2019-06-14 18:34:48 +0300

    Третье задание

--- js/stat.js
+++ js/stat.js
@@ -33,17 +33,18 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   renderText(ctx, 'Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP);
-  renderText(ctx, 'Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP + FONT_GAP);
+  renderText(ctx, 'Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP * 2);
 
   for (var i = 0; i < names.length; i++) {
     var columnHeight = (COLUMN_MAX_HEIGHT * times[i]) / bestResult;
     var colorSaturation = Math.ceil(Math.random() * 100);
+    var columnPositionX = COLUMN_X + (COLUMN_WIDTH + COLUMN_GAP) * i;
 
-    renderText(ctx, Math.round(times[i]), COLUMN_X + (COLUMN_WIDTH + COLUMN_GAP) * i, COLUMN_Y + COLUMN_MAX_HEIGHT - columnHeight - FONT_GAP);
+    renderText(ctx, Math.round(times[i]), columnPositionX, COLUMN_Y + COLUMN_MAX_HEIGHT - columnHeight - FONT_GAP);
 
     ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + colorSaturation + '%, 50%)';
-    ctx.fillRect(COLUMN_X + (COLUMN_WIDTH + COLUMN_GAP) * i, COLUMN_Y + COLUMN_MAX_HEIGHT - columnHeight, COLUMN_WIDTH, columnHeight);
+    ctx.fillRect(columnPositionX, COLUMN_Y + COLUMN_MAX_HEIGHT - columnHeight, COLUMN_WIDTH, columnHeight);
 
-    renderText(ctx, names[i], COLUMN_X + (COLUMN_WIDTH + COLUMN_GAP) * i, COLUMN_Y + COLUMN_MAX_HEIGHT + FONT_GAP);
+    renderText(ctx, names[i], columnPositionX, COLUMN_Y + COLUMN_MAX_HEIGHT + FONT_GAP);
   }
 };
